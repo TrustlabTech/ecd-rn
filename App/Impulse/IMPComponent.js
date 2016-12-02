ab4703f276e552d1a053d631181a01699dd46fcc
Werner Roets <cobolt.exe@gmail.com>
2016-12-02 11:11:09 +0200

    Added logging infrastructure

---
+++ App/Impulse/IMPComponent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default class IMPComponent extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps() {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(preProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+}
