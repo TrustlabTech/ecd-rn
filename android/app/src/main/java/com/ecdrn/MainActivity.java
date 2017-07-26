910386c62bbb85e31a93aef2636b1571dea0ec43
Alberto Dallaporta <alberto@novalab.io>
2017-07-26 11:38:58 +0200

    Fabric logger

--- android/app/src/main/java/com/ecdrn/MainActivity.java
+++ android/app/src/main/java/com/ecdrn/MainActivity.java
@@ -1,6 +1,15 @@
 package com.ecdrn;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+import com.ecdrn.reactnative.FabricLogger;
+import com.facebook.common.logging.FLog;
 import com.facebook.react.ReactActivity;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends ReactActivity {
     /**
@@ -10,5 +19,20 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Ecdrn";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Fabric.with(this, new Crashlytics());
+
+        FabricLogger logger = new FabricLogger().getInstance();
+        logger.setApplicationTag(getPackageName());
+        logger.setMinimumLoggingLevel(Log.INFO);
+
+        FLog.setLoggingDelegate(logger);
     }
 }
