90ee99cbaacfa00887037d74a789c339a4ffa8b5
Alberto Dallaporta <alberto@novalab.io>
2017-07-28 16:50:49 +0200

    Assign/Unassign feature - Removed check on RSA ID - App version bump

--- App/Routes.js
+++ App/Routes.js
@@ -11,6 +11,8 @@ import LoginScene from './Scenes/LoginScene'
 import MainScene from './Scenes/MainScene'
 import HistoryScene from './Scenes/HistoryScene'
 import AddChildScene from './Scenes/AddChildScene'
+import AssignChildScene from './Scenes/AssignChildScene'
+import UnassignChildScene from './Scenes/UnassignChildScene'
 
 /**
  * A list of available scenes to navigate to
@@ -19,6 +21,14 @@ export default {
   addChild: {
     title: 'ECD APP',
     scene: AddChildScene
+  },
+  assignChild: {
+    title: 'ECD APP',
+    scene: AssignChildScene,
+  },
+  unassignChild: {
+    title: 'ECD APP',
+    scene: UnassignChildScene,
   },
   history: {
     title: 'ECD APP',
