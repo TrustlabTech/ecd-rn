e1715c7fae0f2c161019387a50dc99cb7ce63d7c
Zayin Krige <zkrige@gmail.com>
2018-04-09 15:45:36 +0200

    fix: manually link crash reporting and analytics - they werent working previously

--- android/app/src/main/java/com/ecdrn/MainApplication.java
+++ android/app/src/main/java/com/ecdrn/MainApplication.java
@@ -11,6 +11,9 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.multidex.MultiDex;
 import com.facebook.react.ReactPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
@@ -47,6 +50,9 @@ public class MainApplication extends NavigationApplication {
   @Override
   public List<ReactPackage> createAdditionalReactPackages() {
     return Arrays.<ReactPackage>asList(
+            new AppCenterReactNativePackage(MainApplication.this),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, true),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, "ALWAYS"),
             new VectorIconsPackage(),
             new ReactNativePackages(),
             new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
